fe9fbed62883f83e94cfbf61f5264435fffba821
Ruby Kaur <rubykaur@uw.edu>
2023-05-13 15:22:58 -0700

    Pending

--- skiplist.cpp
+++ skiplist.cpp
@@ -8,124 +8,142 @@
 
 using namespace std;
 
-// SNode implementation
-
+// SNode constructor
 SNode::SNode(int val) : val(val) {}
 
+// SNode copy constructor
 SNode::SNode(const SNode &other) : val(other.val), next(other.next) {}
 
-// SkipList implementation
-
-SkipList::SkipList(int levels, int probability) : levels(levels), probability(probability) {
-  head = new SNode();
+// SkipList constructor
+SkipList::SkipList(int levels, int probability)
+    : levels(levels), probability(probability) {
+  head = new SNode(INT_MIN);
   head->next.resize(levels, nullptr);
 }
 
-SkipList::SkipList(const SkipList &other) : levels(other.levels), probability(other.probability) {
-  head = new SNode();
+// SkipList copy constructor
+SkipList::SkipList(const SkipList &other)
+    : levels(other.levels), probability(other.probability) {
+  head = new SNode(INT_MIN);
   head->next.resize(levels, nullptr);
 
-  unordered_map<const SNode *, SNode *> nodeMap;
+  unordered_map<SNode *, SNode *> nodeMap; // Map of original nodes to copied nodes
 
-  SNode *cur = other.head->next[0];
-  SNode *prev = head;
-  while (cur != nullptr) {
-    SNode *newNode = new SNode(*cur);
-    prev->next[0] = newNode;
-    nodeMap[cur] = newNode;
-
-    cur = cur->next[0];
-    prev = newNode;
+  // Copy the nodes at each level
+  SNode *curr = other.head;
+  while (curr) {
+    SNode *newNode = new SNode(*curr);
+    nodeMap[curr] = newNode;
+    curr = curr->next[0];
   }
 
-  for (int i = 1; i < levels; i++) {
-    cur = other.head->next[i];
-    prev = head;
-    while (cur != nullptr) {
-      SNode *newNode = new SNode(*cur);
-      prev->next[i] = newNode;
-      nodeMap[cur] = newNode;
-
-      cur = cur->next[i];
-      prev = newNode;
+  // Connect the copied nodes at each level
+  curr = other.head;
+  while (curr) {
+    SNode *newNode = nodeMap[curr];
+    for (int i = 0; i < levels; i++) {
+      if (curr->next[i]) {
+        newNode->next[i] = nodeMap[curr->next[i]];
+      }
     }
-  }
-
-  for (int i = 0; i < levels; i++) {
-    cur = head->next[i];
-    while (cur != nullptr) {
-      cur->next[i] = nodeMap[cur->next[i]];
-      cur = cur->next[i];
-    }
+    curr = curr->next[0];
   }
 }
 
+// SkipList destructor
 SkipList::~SkipList() {
-  SNode *cur = head;
-  while (cur != nullptr) {
-    SNode *next = cur->next[0];
-    delete cur;
-    cur = next;
+  SNode *curr = head;
+  while (curr) {
+    SNode *next = curr->next[0];
+    delete curr;
+    curr = next;
   }
 }
 
+// Add a single value to the SkipList
 void SkipList::add(int val) {
-  vector<SNode *> prevNodes = getBeforeNodes(val);
+  vector<SNode *> beforeNodes = getBeforeNodes(val);
 
   SNode *newNode = new SNode(val);
-  int newNodeLevels = 1;
+  newNode->next.resize(levels, nullptr);
 
-  while (shouldInsertAtHigherLevel()) {
-    newNodeLevels++;
-  }
+  for (int i = 0; i < levels; i++) {
+    newNode->next[i] = beforeNodes[i]->next[i];
+    beforeNodes[i]->next[i] = newNode;
 
-  newNode->next.resize(newNodeLevels, nullptr);
-
-  for (int i = 0; i < newNodeLevels; i++) {
-    newNode->next[i] = prevNodes[i]->next[i];
-    prevNodes[i]->next[i] = newNode;
+    if (!shouldInsertAtHigherLevel()) {
+      break;
+    }
   }
 }
 
+// Add multiple values to the SkipList
 void SkipList::add(const vector<int> &values) {
   for (int val : values) {
     add(val);
   }
 }
 
+// Remove a value from the SkipList
 bool SkipList::remove(int val) {
-  vector<SNode *> prevNodes = getBeforeNodes(val);
+  vector<SNode *> beforeNodes = getBeforeNodes(val);
 
-  SNode *targetNode = prevNodes[0]->next[0];
+  SNode *target = beforeNodes[0]->next[0];
 
-  if (targetNode == nullptr || targetNode->val != val) {
-    return false;
+  if (!target || target->val != val) {
+    return false; // Value not found
   }
 
-  int targetNodeLevels = targetNode->next.size();
-
-  for (int i = 0; i < targetNodeLevels; i++) {
-    prevNodes[i]->next[i] = targetNode->next[i];
+  for (int i = 0; i < levels; i++) {
+    if (beforeNodes[i]->next[i] != target) {
+      break;
+    }
+    beforeNodes[i]->next[i] = target->next[i];
   }
 
-  delete targetNode;
-
+  delete target;
   return true;
 }
 
+// Check if a value is present in the SkipList
 bool SkipList::contains(int val) const {
-  SNode *cur = head;
-
-  for (int i = levels - 1; i >=
-
-
-
-
-
-
-
-// checks if the value needs to be inserted at a higher level
-bool SkipList::shouldInsertAtHigherLevel() const {
-  return probability >= Random::random() % 100;
+  SNode *curr = head;
+  for (int i = levels - 1; i >= 0; i--) {
+    while (curr->next[i] && curr->next[i]->val < val) {
+      curr = curr->next[i]; 
+    }
+  return (curr->next[0] && curr->next[0]->val == val);
 }
 
+// Get the nodes before the insertion point for a given value
+vector<SNode *> SkipList::getBeforeNodes(int val) const {
+  vector<SNode *> beforeNodes(levels, nullptr);
+
+  SNode *curr = head;
+  for (int i = levels - 1; i >= 0; i--) {
+    while (curr->next[i] && curr->next[i]->val < val) {
+      curr = curr->next[i];
+    }
+    beforeNodes[i] = curr;
+  }
+
+  return beforeNodes;
+}
+
+// Check if a node should be inserted at a higher level
+bool SkipList::shouldInsertAtHigherLevel() const {
+  return (randomChance() < probability);
+}
+
+// Print the contents of the SkipList
+void SkipList::print() const {
+  for (int i = levels - 1; i >= 0; i--) {
+    SNode *curr = head->next[i];
+    cout << "Level " << i << ": ";
+    while (curr) {
+      cout << curr->val << " ";
+      curr = curr->next[i];
+    }
+    cout << endl;
+  }
+}
